2d7d0c0cea289092095350e5646730474f410890
Rieza Eka Tomara <riezaekatomara@gmail.com>
2025-03-17 13:51:52 +0700

    add main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./styles/tailwind.css";
 import Home from "./pages/Home.jsx";
 import Movie from "./pages/Movie.jsx";
-import Register from "./pages/Register.jsx";
-import Register2 from "./pages/Register2.jsx";
 import Login from "./pages/Login.jsx";
 import MovieDetails from "./pages/Movie-Details.jsx";
 // import SeatSelectionPage from "./pages/Seat.jsx";
@@ -16,8 +14,6 @@ function App() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/movie" element={<Movie />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/register2" element={<Register2 />} />
       <Route path="/login" element={<Login />} />
       <Route path="/movie-details" element={<MovieDetails />} />
       {/* <Route path="/seat" element={<SeatSelectionPage />} />
